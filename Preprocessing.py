dae6561dc17f9321df88bcd3289ef97e90f3321a
LC70716 <contileonardo59@gmail.com>
2024-05-21 09:22:14 +0200

    added noising

--- Preprocessing.py
+++ Preprocessing.py
@@ -22,8 +22,15 @@ def PlotAreaOfInterest(nii_data, denominator):
         plt.imshow(nii_data[:, :, slice], cmap="gray")
         plt.show()
 
+def AddNoise(image,std): #THE ACTUAL IMPLEMENTATION MAY NEED TO DIFFER
+    noisy = image
+    for row in range(np.size(noisy,0)):
+        for col in range(np.size(noisy,1)):
+            if noisy[row,col] != 0:
+               noisy[row,col] += np.random.normal(0.0,std)
+    return noisy
 
-def Nifti2JPG(inputPath, outputPath, denominator=2, size=10):
+def Nifti2JPG(inputPath, outputPath, denominator=2, size=10, noise = False):
     """Generates JPG files given nii.gz files
 
     Parameters
@@ -36,8 +43,13 @@ def Nifti2JPG(inputPath, outputPath, denominator=2, size=10):
        slice from where the selection given by range starts (# slice = # slices/denominator)
     size : UInt
        number of slices (the interval is 2*size) before and after the slice pointed by denominator which are to be generated into JPG
+    noise : Bool
+        if true adds gaussian noise w/ mean and std
+    std_weigth : float
+        the std deviation is computed as follows : gray scale value * std_weigth
     """
     counter = 0
+    frac_snr_tot=np.array([])
     for dirpath, dirnames, filenames in os.walk(inputPath):
         for filename in filenames:
             print(os.path.join(dirpath, filename))
@@ -54,24 +66,29 @@ def Nifti2JPG(inputPath, outputPath, denominator=2, size=10):
                     if ((number_of_slices // denominator) - size) >= 0 and (
                         (number_of_slices // denominator) + size
                     ) < number_of_slices:
-                        nii_data[:, :, slice] = cv2.normalize(
-                            nii_data[:, :, slice],
-                            None,
-                            255,
-                            0,
-                            cv2.NORM_MINMAX,
-                            cv2.CV_8U,
-                        )  # each slice is normalized separately
+                        if (noise == True):
+                            std = np.nanstd(np.where(np.isclose(nii_data[:, :, slice],0), np.nan, nii_data[:, :, slice]))
+                            noise_std = std*np.sqrt(3)
+                            old_snr_tot = np.nanmean(np.where(np.isclose(nii_data[:, :, slice],0), np.nan, nii_data[:, :, slice]))/std
+                            nii_data[:, :, slice] = AddNoise(nii_data[:, :, slice],noise_std)
+                            new_snr_tot = np.nanmean(np.where(np.isclose(nii_data[:, :, slice],0), np.nan, nii_data[:, :, slice]))/np.nanstd(np.where(np.isclose(nii_data[:, :, slice],0), np.nan, nii_data[:, :, slice]))
+                            nii_data[:,:,slice] = np.where(nii_data[:, :, slice] == np.nan, 0, nii_data[:, :, slice])
+                            frac_snr_tot=np.append(frac_snr_tot,old_snr_tot/new_snr_tot)
+                        min = np.min(nii_data[:,:,slice])
+                        max = np.max(nii_data[:,:,slice])
+                        nii_data[:,:,slice] = np.uint8(np.floor(((nii_data[:,:,slice] - min)/(max - min))*255))
                         cv2.imwrite(
                             outputPath + str(counter) + str(slice) + ".jpg",
                             nii_data[:, :, slice],
                         )
                 counter += 1
+                print("mean snr ratio:" + str(np.mean(frac_snr_tot)))
 
 
 Nifti2JPG(
-    inputPath="C:/Users/conti/Desktop/Progetto_Pattern/DataSets/DS_A_PIOP2",
-    outputPath="C:/Users/conti/Desktop/Progetto_Pattern/Train/TrainA",
+    inputPath="C:/Users/conti/Desktop/Progetto_Pattern/DataSets/DS_B_PIOP2",
+    outputPath="C:/Users/conti/Desktop/Progetto_Pattern/PIOP2/TrainBALPHA/",
+    noise=True,
 )
-
+  
 # %%
